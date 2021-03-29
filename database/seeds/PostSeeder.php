c68d90fe630047f01b6926400dff43c3cdfb8ba2
Diego <diego.scano77@gmail.com>
2021-03-29 21:47:04 +0200

    base complete

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Author;
+use App\AuthorDetail;
+use App\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++) {
+          $author = new Author();
+          $author->name = $faker->firstName();
+          $author->surname = $faker->lastName();
+          $author->email = $faker->email();
+          $author->save();
+
+          $authorDetail = new AuthorDetail();
+          $authorDetail->bio = $faker->text();
+          $authorDetail->website = $faker->url();
+          $authorDetail->pic = 'https://picsum.photos/seed/' . rand(0, 1000) . '/200/300';
+
+          $author->detail()->save($authorDetail);
+
+          for($x = 0; $x < rand(1, 5); $x++) {
+            $post = new Post();
+            $post->title = $faker->text(10);
+            $post->body = $faker->text(1000);
+            $author->posts()->save($post);
+          }
+        }
+    }
+}
---
+++ resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+  <head>
+    <title>Index</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+  </head>
+
+  <body>
+
+    <div class="container">
+      @yield('content')
+    </div>
+
+  </body>
+</html>
